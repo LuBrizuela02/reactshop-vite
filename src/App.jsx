8766bfbff26e23836d7ea27eb761560ff8ac5d29
Ludmila Brizuela <brizuelal127@gmail.com>
2025-09-15 01:44:22 -0300

    Segundo commit: agrego modificaciones para subir a GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './componentes/header/Header'
 import Home from './componentes/home/Home'
 import DetalleProductos from './componentes/productos/DetalleProductos'
